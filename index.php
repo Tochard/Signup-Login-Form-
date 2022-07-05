be3ed44ed086f926dabfe550d49477467241dbe0
Ebube Anthony Egolum <egolumaer@gmail.com>
2022-07-05 21:18:06 +0100

    index.php added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp | Tochard Code-Planet</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+
+    <!-- <link rel="stylesheet" type="text/css" href="file:///C:\xampp\htdocs\ChatApp\css/all.css"> -->
+</head>
+
+<body>
+    
+<div class="wrapper">
+	<section class="form signup">
+		<header>SignUp or Login</header>
+
+
+		<div class="link">Have an account? <a href="login.php">Login Now</a></div>
+        <div class="link">Already signed Up? <a href="sigup.php">Login Now</a></div>
+		
+	</section>
+	
+</div>
+
+
+
+</body>
+</html> 
